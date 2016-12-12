2e9ff31cc042da88b3f98a4a347a5f6d1dd86f17
leobrowning92 <leobrowning92@gmail.com>
2016-12-12 16:34:47 +1300

    split out the rendering function for readibility

---
+++ random_line.py
@@ -0,0 +1,50 @@
+import os
+import cairo as cairo
+import numpy as np
+from render import Animate
+
+class Particle(object):
+    def __init__(self,x,y,velocity):
+
+        self.position = np.array([x,y])
+        self.velocity = velocity
+        assert len(self.velocity)==2
+
+
+    def move(self):
+        self.position= np.add(self.velocity, self.position)
+    def accelerate(self,a):
+        self.velocity=np.add(self.velocity,a)
+
+
+
+
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+    # These are the required arguments for the Animation
+    BACK = [1, 1, 1, 1]
+    FRONT = [149/255, 131/255, 189/255, 1]
+    SIZE = 1000
+    UNIT=1.0/SIZE
+    number=10 # number of particles in the system
+    particles=[None]*number
+    for i in range(number):
+         particles[i] = Particle(0.5+i/20, 0.5, [0,0])
+
+
+    def step(self):
+        # render.clear_canvas()
+        for particle in particles:
+            pos=particle.position
+            particle.move()
+            render.circle(pos[0],pos[1],2*render.pix)
+            particle.accelerate([0,UNIT/10])
+        return True
+
+
+    # These are the bits that need to be run when calling the Animation
+    render = Animate(SIZE, BACK, FRONT,100,step)
+    render.start()
