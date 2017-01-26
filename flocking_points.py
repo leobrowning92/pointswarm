06df2c80543810a8d0ab1815f789488b5af11d3a
Leo Browning <leobrowning92@gmail.com>
2017-01-26 16:01:46 +1300

    working flocking , but a couple of issues need debugging
    look at evolution of nan velocities, and then positions over time.
    also look at modifying relative velocities to keep things on the page,
    possibly also look into edge collision

--- flocking_points.py
+++ flocking_points.py
@@ -29,7 +29,7 @@ class BoidFlock(object):
     """The boid flock is a collection of Particle objects with the added benifit of being able to have some flocking methods applied to it as a whole"""
     def __init__(self,positions,velocities,scalings,ranges,unit):
         assert len(positions)==len(velocities) , "the number of positions and velocities do not match"
-        self.flock=[Particle(positions[i][0],positions[i][0],velocities[i]) for i in range(len(positions))]
+        self.boids=[Particle(positions[i][0],positions[i][1],velocities[i]) for i in range(len(positions))]
 
         self.seperation_scaling=scalings[0]
         self.alignment_scaling=scalings[1]
@@ -43,41 +43,43 @@ class BoidFlock(object):
 
 
     def move_flock(self):
-        for boid in self.flock:
+        for boid in self.boids:
             boid.move()
             v1 = self.get_seperation_velocity(boid)
             v2 = self.get_alignment_velocity(boid)
             v3 = self.get_com_velocity(boid)
             boid.accelerate((v1+v2+v3))
 
-            
+
+
 
     def get_seperation_velocity(self,target_boid):
         """probably in this case the range should be >10x the scaling so as to have smothe acceleration away from one another"""
         seperation_vector=np.array([0,0])
-        for boid in self.flock:
+        for boid in self.boids:
             if boid !=target_boid:
                 if np.linalg.norm(boid.position-target_boid.position) < self.seperation_range:
                     boid_difference=target_boid.position-boid.position
 
                     #the 1/norm**2 term gives i/r scaling of this vector
-                    seperation_vector=seperation_vector + (boid_difference)/(np.linalg.norm(boid_difference)**2)
-        return seperation_vector * self.seperation_scaling
+                    seperation_vector=seperation_vector + (boid_difference)/(np.linalg.norm(boid_difference))
+        return seperation_vector * self.seperation_scaling *self.unit
 
 
     def get_alignment_velocity(self,target_boid):
         """the alignment velocity is a scaled unit vector, and is not scaled proportional to how different the target boids velocity is from the alignment velocity"""
         group_velocity = np.array([0,0])
-        for boid in self.flock:
+        for boid in self.boids:
             if boid !=target_boid:
                 if np.linalg.norm(boid.position-target_boid.position) < self.alignment_range:
                     group_velocity=np.add(boid.velocity,group_velocity)
+
         return group_velocity * self.alignment_scaling * self.unit / np.linalg.norm(group_velocity)
 
     def get_com_velocity(self,target_boid):
         """the COM velocity is a scaled unit vector, and is not scaled proportional to how different the target boids positions is from the COM"""
         centre_of_mass = np.array([0,0])
-        for boid in self.flock:
+        for boid in self.boids:
             if boid !=target_boid:
                 if np.linalg.norm(boid.position-target_boid.position) < self.com_scaling:
                     centre_of_mass=np.add(boid.position,centre_of_mass)
@@ -93,31 +95,32 @@ if __name__ == '__main__':
     background_color = [1, 1, 1, 1]
     foreground_color = [(149/255, 131/255, 189/255, 0.01)]
     image_size = 500
-    UNIT=1.0/image_size
-    number=10# number of particles in the system
-    particles=[None]*number
+    unit=1.0/image_size
+    number=20# number of particles in the system
+    positions=[None]*number
+    velocities=[None]*number
     for i in range(number):
-         particles[i] = Particle(0.5/number+i/number, np.random.uniform(0,1), [0,0])
+        positions[i] = [np.random.uniform(0,1), np.random.uniform(0,1)]
+        velocities[i] = [np.random.uniform(0,1)*unit, np.random.uniform(0,1)*unit]
 
-    total_steps=200
+
+    total_steps=5
 
     cm_subsection = np.linspace(1,0, total_steps)
-    foreground_colors = [ cm.plasma(x,alpha=0.05) for x in cm_subsection ]
+    foreground_colors = [ cm.plasma(x,alpha=1) for x in cm_subsection ]
 
-
+    flock = BoidFlock(positions, velocities, [0.1,0.1,0.1], np.multiply(unit,[10,100,100]),unit)
 
     def step_function(self):
+        print("step")
+        #print(flock.boids[1].position,flock.boids[1].velocity)
         # render.clear_canvas()
-        for primary_particle in particles:
-            primary_particle.move()
-            self.circle(primary_particle.position[0], primary_particle.position[1], 2*self.pix)
-            for other in particles:
-                #this code loop is for the particle-particle interractions
-
-                pass
-
-
-
+        flock.move_flock()
+        for boid in flock.boids:
+            print(boid.position,boid.velocity)
+            pos=boid.position
+            self.circle(pos[0],pos[1],10*self.unit)
+            #boid.accelerate(random_accelerator(unit/5))
         return True
 
 
