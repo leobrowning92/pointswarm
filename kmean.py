9bd6ddc07dd77e6e19631dadf53590402f183096
Leo Browning <leobrowning92@gmail.com>
2017-02-13 13:53:11 +1300

    added array reshape to an n*m x 3 array of pixel colors

--- kmean.py
+++ kmean.py
@@ -6,6 +6,7 @@ import numpy as np
 
 def imageto_array(image_path,v=False):
     im = Image.open(image_path)
+    im.thumbnail((256,256))
     arrayim =  np.asarray(im)
     if v:
         print(im.size,im.format,im.mode)
@@ -13,4 +14,9 @@ def imageto_array(image_path,v=False):
     return arrayim
 
 
-im=imageto_array("testimage.jpg",v=True)
+
+
+image_array = imageto_array("testimage.jpg",v=True)
+h=image_array.shape[0]
+w=image_array.shape[1]
+image_list=np.reshape(image_array,((h*w),3)).shape
