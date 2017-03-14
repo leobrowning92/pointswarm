277de19187c4c1863dfa97e5349e07eced9c94be
Leo Browning <leobrowning92@gmail.com>
2017-03-14 16:29:42 +1300

    kmean functions
    kmean shows the clustered colors at the bottom of the image

--- kmean.py
+++ kmean.py
@@ -2,7 +2,7 @@ from PIL import Image
 import numpy as np
 from render import Show
 import os
-from scipy.cluster.vq import kmeans
+from scipy.cluster.vq import kmeans2
 
 
 
@@ -27,31 +27,39 @@ def open_show(image_path,v=False):
         for i in range(h):
             for j in range(w):
                 cr.set_source_rgb(*image_array[i,j])
-                #cr.translate(w/2, h/2)
                 cr.rectangle(j, i, 1, 1)
                 cr.fill()
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
     app = Show(draw,[w,h])
 
-def open_kmean(image_path,clusters=5,v=False):
+def open_kmean(image_path,clusters=5,v=False,show=False):
     image_array = imageto_array(image_path,v=False)/256
     h=image_array.shape[0]
     w=image_array.shape[1]
     image_list=np.reshape(image_array,((h*w),3))
-    print(kmeans(image_list,clusters))
-    def draw(self,cr):
-        for i in range(h):
-            for j in range(w):
-                cr.set_source_rgb(*image_array[i,j])
-                #cr.translate(w/2, h/2)
-                cr.rectangle(j, i, 1, 1)
+    centers=kmeans2(image_list,clusters)
+    if v:
+        print(centers)
+    if show:
+        def draw(self,cr):
+            #Draw the image
+            for i in range(h):
+                for j in range(w):
+                    cr.set_source_rgb(*image_array[i,j])
+                    cr.rectangle(j, i, 1, 1)
+                    cr.fill()
+            #Draw the cluster centers
+            for i in range(len(centers[0])):
+                cr.set_source_rgb(*centers[0][i])
+                cr.rectangle(i*w/clusters, h, w/clusters, h*0.1)
                 cr.fill()
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    app = Show(draw,[w,h])
+
+        app = Show(draw,[w,h*1.1])
+    return centers[0]
 
 
 if __name__ == '__main__':
 
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-    open_show("testimage.jpg")
+    open_kmean("testimage.jpg",clusters=5,show=True,v=True)
